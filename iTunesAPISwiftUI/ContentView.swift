6ff22e7b61c7bae08c51794acbc5696fcabd0b40
Seyfeddin Başsaraç <seyfeddin@wearethread.co>
2019-11-23 16:50:03 +0300

    Initial Commit

---
+++ iTunesAPISwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iTunesAPISwiftUI
+//
+//  Created by Seyfeddin Bassarac on 23.11.2019.
+//  Copyright © 2019 ThreadCo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
