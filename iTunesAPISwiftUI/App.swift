574cc0fdd2dd90b8171b9c79a2b14475258cfa05
Seyfeddin Başsaraç <seyfeddin@wearethread.co>
2020-06-25 01:39:23 +0300

    Migrate to iOS14.0

---
+++ iTunesAPISwiftUI/App.swift
@@ -0,0 +1,18 @@
+//
+//  App.swift
+//  iTunesAPISwiftUI
+//
+//  Created by Seyfeddin Bassarac on 25.06.2020.
+//  Copyright © 2020 ThreadCo. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct iTunesAPISwiftUI: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
