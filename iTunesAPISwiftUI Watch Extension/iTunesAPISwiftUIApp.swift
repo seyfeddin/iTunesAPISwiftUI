dac3b8754b10170c568b8b5f181d0453b0a15acb
Seyfeddin Başsaraç <seyfeddin@wearethread.co>
2021-09-25 18:29:08 +0300

    Add watch os target

---
+++ iTunesAPISwiftUI Watch Extension/iTunesAPISwiftUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  iTunesAPISwiftUIApp.swift
+//  iTunesAPISwiftUI Watch Extension
+//
+//  Created by Seyfeddin Bassarac on 25.09.2021.
+//  Copyright © 2021 ThreadCo. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct iTunesAPISwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
